e1a93538610c5853330bcb5cdf0b775c91eff64a
Robert Kaufman <rob.m.kaufman@gmail.com>
2010-10-30 17:39:49 -0400

    try to validate card input for new deck. doesn't work

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -2,7 +2,10 @@ class Deck < ActiveRecord::Base
   has_many :cards
   accepts_nested_attributes_for :cards
 
+  validates_format_of :bulk_cards, :with => /\A(.*\|.*\n?){1,}\Z/
+
   def bulk_cards
+    cards.collect{|c| "#{c.front}|#{c.back}"}.join("\n")
   end
 
   def bulk_cards=(text)
--- test/unit/deck_test.rb
+++ test/unit/deck_test.rb
@@ -1,8 +1,14 @@
 require 'test_helper'
 
 class DeckTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "create new cards along with a new deck" do
+    before_cards = Card.count
+    d = Deck.create(:title => "some title", :bulk_cards => "one|two\nthree|four")
+    assert_equal before_cards + 2, Card.count
+  end
+
+  test "new deck should reject malformed card text" do
+    d = Deck.create(:title => "some title", :bulk_cards => "one|two\nthree four") # note: would need a '|'
+    assert !d.valid?
   end
 end
