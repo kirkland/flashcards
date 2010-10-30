d9da6447b39aad7132427c155407613abf697809
Robert Kaufman <rob.m.kaufman@gmail.com>
2010-10-30 17:08:06 -0400

    for new deck, use bulk entry form

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,4 +1,15 @@
 class Deck < ActiveRecord::Base
   has_many :cards
   accepts_nested_attributes_for :cards
+
+  def bulk_cards
+  end
+
+  def bulk_cards=(text)
+    cards = text.split("\n")
+    cards.each do |card|
+      front, back = card.split("|")
+      Card.create(:deck => self, :front => front, :back => back)
+    end
+  end
 end
