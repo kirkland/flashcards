4f7a05c386df37a005bed224ec57a3f69cf58ae9
Robert Kaufman <rob.m.kaufman@gmail.com>
2010-10-30 18:36:51 -0400

    properly validate cards on new deck

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -2,13 +2,14 @@ class Deck < ActiveRecord::Base
   has_many :cards
   accepts_nested_attributes_for :cards
 
-  validates_format_of :bulk_cards, :with => /\A(.*\|.*\n?){1,}\Z/
-
+  validates_format_of :bulk_cards, :with => /\A(.+\|.+\n?){1,}\z/
+          
   def bulk_cards
-    cards.collect{|c| "#{c.front}|#{c.back}"}.join("\n")
+    @bulk_cards || ""
   end
 
   def bulk_cards=(text)
+    @bulk_cards ||= text
     cards = text.split("\n")
     cards.each do |card|
       front, back = card.split("|")
