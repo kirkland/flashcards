5f65ad84f2479a37157d4664543a884349dcb743
Robert Kaufman <rob.m.kaufman@gmail.com>
2010-11-24 20:28:37 -0500

    display flash notices, kind of crummily

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
       flash[:notice] = "Registration successful."  
       redirect_to root_url  
     else  
+      flash[:error] = @user.errors
       render :action => 'new'  
     end  
   end
