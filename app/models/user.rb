ff191ffbc7c2635172cd946b9a26ae4a3613aa46
Robert Kaufman <rob.m.kaufman@gmail.com>
2010-12-19 14:27:06 -0500

    make master score slightly easier

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ActiveRecord::Base
   # consider only 5 most recent quizzes, and if < 5, do as if scored 0 on some
   def deck_mastery(deck_id, back_to_front)
     qs = quizzes.where(:deck_id => deck_id, :back_to_front => back_to_front).order(:created_at).last(5)
-    (qs.collect{|x| x.quiz_cards.correct.count}.sum.to_f / 5 / Deck.find(deck_id).cards.count * 10).floor
+    (qs.collect{|x| x.quiz_cards.correct.count}.sum.to_f / 5 / Deck.find(deck_id).cards.count * 10).round
   end
 end
