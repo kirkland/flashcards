d8805af600952dbd0a696f0b488666430a523b8d
Robert Kaufman <rob.m.kaufman@gmail.com>
2010-12-19 13:28:16 -0500

    show simple way to score 0-10 for each quiz

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   acts_as_authentic
 
   has_many :quizzes
+
+  def deck_mastery(deck_id, back_to_front=false)
+    (quizzes.where(:deck_id => deck_id, :back_to_front => back_to_front).collect{|x| x.quiz_cards.correct.count}.max.to_f / Deck.find(deck_id).cards.count * 10).floor
+  end
 end
