4bf78a44a120141c13403871b9771746f7d076da
Robert Kaufman <rob.m.kaufman@gmail.com>
2010-12-19 13:56:35 -0500

    more sophisticated mastery score, based on average score of last 5 tries

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ActiveRecord::Base
 
   has_many :quizzes
 
-  def deck_mastery(deck_id, back_to_front=false)
-    (quizzes.where(:deck_id => deck_id, :back_to_front => back_to_front).collect{|x| x.quiz_cards.correct.count}.max.to_f / Deck.find(deck_id).cards.count * 10).floor
+  # average percent correct, on a scale of 0-10, rounded down
+  # consider only 5 most recent quizzes, and if < 5, do as if scored 0 on some
+  def deck_mastery(deck_id, back_to_front)
+    qs = quizzes.where(:deck_id => deck_id, :back_to_front => back_to_front).order(:created_at).last(5)
+    (qs.collect{|x| x.quiz_cards.correct.count}.sum.to_f / 5 / Deck.find(deck_id).cards.count * 10).floor
   end
 end
