6620e12b142e60af1c63349b0dba43e85082c848
Robert Kaufman <rob.m.kaufman@gmail.com>
2010-12-20 19:04:02 -0500

    styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,14 @@ class User < ActiveRecord::Base
 
   has_many :quizzes
 
-  # average percent correct, on a scale of 0-10, rounded down
-  # consider only 5 most recent quizzes, and if < 5, do as if scored 0 on some
-  def deck_mastery(deck_id, back_to_front)
+  # best score from #count last tries
+  def best_score(deck_id, back_to_front)
     qs = quizzes.where(:deck_id => deck_id, :back_to_front => back_to_front).order(:created_at).last(5)
-    (qs.collect{|x| x.quiz_cards.correct.count}.sum.to_f / 5 / Deck.find(deck_id).cards.count * 10).round
+    qs.collect{|x| x.quiz_cards.correct.count}.max
+  end
+
+  def worst_score(deck_id, back_to_front)
+    qs = quizzes.where(:deck_id => deck_id, :back_to_front => back_to_front).order(:created_at).last(5)
+    qs.collect{|x| x.quiz_cards.correct.count}.min
   end
 end
