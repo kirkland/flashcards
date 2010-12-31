2354755a7f05a74953c53657da6227f9a8b3c6d6
Robert Kaufman <rob.m.kaufman@gmail.com>
2010-12-31 15:37:29 -0500

    show last 5 scores instead of best/worst

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ActiveRecord::Base
 
   has_many :quizzes
 
+  def last_five_scores(deck_id, back_to_front)
+    qs = quizzes.where(:deck_id => deck_id, :back_to_front => back_to_front).order(:created_at).last(5).collect{|x| x.quiz_cards.correct.count}.join(", ")
+  end
+
   # best score from #count last tries
   def best_score(deck_id, back_to_front)
     qs = quizzes.where(:deck_id => deck_id, :back_to_front => back_to_front).order(:created_at).last(5)
