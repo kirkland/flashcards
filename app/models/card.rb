3605456acc93935d9b4f5b6b68ef5c96cd81129f
Robert Kaufman <rob.m.kaufman@gmail.com>
2011-01-01 16:06:40 -0500

    save correct filename

--- app/models/card.rb
+++ app/models/card.rb
@@ -6,7 +6,7 @@ class Card < ActiveRecord::Base
     :storage => :s3,
     :s3_credentials => S3_CREDENTIALS, 
     :bucket => S3_BUCKET, 
-    :path => "ohnoes"
+    :path => "card_sounds/:id.:extension"
 
   def should_destroy?
     should_destroy.to_i == 1
