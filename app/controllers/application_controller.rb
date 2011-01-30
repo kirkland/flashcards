9491c29c297d667b62d67a77e4c626d718b43d5c
Robert Kaufman <rob.m.kaufman@gmail.com>
2011-01-30 15:58:08 -0500

    use mobile_view instead

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  has_mobile_fu
+
   helper_method :current_user
 
   private
--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -1,6 +1,4 @@
 class ReviewController < ApplicationController
-  before_filter :find_browser_type
-
   def index
     @decks = params[:all] ? Deck.all : Deck.where(:active => true)
   end
@@ -8,11 +6,5 @@ class ReviewController < ApplicationController
   def show
     @deck_data = Deck.find(params[:id]).review_data.to_json
     @show = params[:show].present? ? params[:show] : "front"
-  end
-
-  private
-
-  def find_browser_type
-    @browser_type = request.user_agent =~ /Android/ ? "phone" : "regular"
   end
 end
