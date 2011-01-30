0f492e3e646b87765ec45df141e3a22068784520
Robert Kaufman <rob.m.kaufman@gmail.com>
2011-01-30 16:16:26 -0500

    do my own mobile thing, steal a regex from mobile_fu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,14 @@ class ApplicationController < ActionController::Base
 
   has_mobile_fu
 
-  helper_method :current_user
+  helper_method :current_user, :is_mobile?
+
+  MOBILE_USER_AGENTS =  'palm|blackberry|nokia|phone|midp|mobi|symbian|chtml|ericsson|minimo|' +
+                        'audiovox|motorola|samsung|telit|upg1|windows ce|ucweb|astel|plucker|' +
+                        'x320|x240|j2me|sgh|portable|sprint|docomo|kddi|softbank|android|mmp|' +
+                        'pdxgw|netfront|xiino|vodafone|portalmmm|sagem|mot-|sie-|ipod|up\\.b|' +
+                        'webos|amoi|novarra|cdm|alcatel|pocket|ipad|iphone|mobileexplorer|' +
+                        'mobile'
 
   private
 
@@ -14,5 +21,9 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user = current_user_session && current_user_session.record
+  end
+
+  def is_mobile?
+    request.user_agent.to_s.downcase =~ Regexp.new(MOBILE_USER_AGENTS)
   end
 end
