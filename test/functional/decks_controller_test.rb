3e089a5433d4b7902d39ea5efc99e210bd5663b6
Robert Kaufman <rob.m.kaufman@gmail.com>
2011-04-02 18:04:26 -0400

    remove nonsensical tests

--- test/functional/decks_controller_test.rb
+++ test/functional/decks_controller_test.rb
@@ -10,40 +10,4 @@ class DecksControllerTest < ActionController::TestCase
     assert_response :success
     assert_not_nil assigns(:decks)
   end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create deck" do
-    assert_difference('Deck.count') do
-      post :create, :deck => @deck.attributes
-    end
-
-    assert_redirected_to deck_path(assigns(:deck))
-  end
-
-  test "should show deck" do
-    get :show, :id => @deck.to_param
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, :id => @deck.to_param
-    assert_response :success
-  end
-
-  test "should update deck" do
-    put :update, :id => @deck.to_param, :deck => @deck.attributes
-    assert_redirected_to deck_path(assigns(:deck))
-  end
-
-  test "should destroy deck" do
-    assert_difference('Deck.count', -1) do
-      delete :destroy, :id => @deck.to_param
-    end
-
-    assert_redirected_to decks_path
-  end
 end
