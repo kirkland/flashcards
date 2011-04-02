8efe8c7c8914d72b99196cae5713f293db12c065
Robert Kaufman <rob.m.kaufman@gmail.com>
2011-04-02 18:35:41 -0400

    make sure an active deck shows up in index

--- test/functional/decks_controller_test.rb
+++ test/functional/decks_controller_test.rb
@@ -2,12 +2,17 @@ require 'test_helper'
 
 class DecksControllerTest < ActionController::TestCase
   setup do
-    @deck = Deck.make
   end
 
   test "should get index" do
     get :index
     assert_response :success
     assert_not_nil assigns(:decks)
+  end
+
+  test "should show an active deck" do
+    @deck = Deck.make(:active => true)
+    get :index
+    assert_select("#deck_#{@deck.id}")
   end
 end
