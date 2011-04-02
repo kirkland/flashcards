2ea25d63f2dca0d9cf58e57afd9a5eaeeca3253a
Robert Kaufman <rob.m.kaufman@gmail.com>
2011-04-02 19:07:25 -0400

    get test to pass by showing ugly, nonfunctional hide button

--- test/functional/logged_out_decks_controller_test.rb
+++ test/functional/logged_out_decks_controller_test.rb
@@ -7,14 +7,20 @@ class LoggedOutDecksControllerTest < ActionController::TestCase
     @deck = Deck.make(:active => true)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
     assert_not_nil assigns(:decks)
   end
 
-  test "should show an active deck" do
+  test 'should show an active deck' do
     get :index
     assert_select("#deck_#{@deck.id}")
   end
+
+  # how do i test that an element does not exist!?
+#  test 'should not show link to hide deck' do
+#    get :index
+#    assert_select("#deck_#{@deck.id} .hide_deck")
+#  end
 end
