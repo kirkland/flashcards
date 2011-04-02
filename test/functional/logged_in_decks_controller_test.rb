093156d79ece5dcc480dcd083a53a3fb6459ec8a
Robert Kaufman <rob.m.kaufman@gmail.com>
2011-04-02 18:56:59 -0400

    make a logged-in user, then check visibility of hide deck link

--- test/functional/logged_in_decks_controller_test.rb
+++ test/functional/logged_in_decks_controller_test.rb
@@ -1,4 +1,17 @@
 require 'test_helper'
 
 class LoggedInDecksControllerTest < ActionController::TestCase
+  tests DecksController
+
+  setup do
+    @deck = Deck.make(:active => true)
+
+    @user = User.make
+    @user_session = UserSession.create(@user)
+  end
+
+  test "should see link to hide deck" do
+    get :index
+    assert_select("#deck_#{@deck.id} .hide_deck")
+  end
 end
--- test/functional/logged_out_decks_controller_test.rb
+++ test/functional/logged_out_decks_controller_test.rb
@@ -3,6 +3,10 @@ require 'test_helper'
 class LoggedOutDecksControllerTest < ActionController::TestCase
   tests DecksController
 
+  setup do
+    @deck = Deck.make(:active => true)
+  end
+
   test "should get index" do
     get :index
     assert_response :success
@@ -10,7 +14,6 @@ class LoggedOutDecksControllerTest < ActionController::TestCase
   end
 
   test "should show an active deck" do
-    @deck = Deck.make(:active => true)
     get :index
     assert_select("#deck_#{@deck.id}")
   end
