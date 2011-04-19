a8b66828b16741372c8b22a1accbc693a8793b3a
Robert Kaufman <rob.m.kaufman@gmail.com>
2011-04-18 22:28:32 -0400

    google analytics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def js_init
+    google_analytics_init
+  end
+
+  def google_analytics_init
+    "var _gaq = _gaq || [];_gaq.push(['_setAccount', 'UA-20647561-2']);_gaq.push(['_trackPageview']);" 
+  end
+
+  def js_dom_loaded
+    js_ga_load
+  end
+
+  def js_ga_load
+    "gaLoad();"
+  end
+
+  def js_after_yield
+  end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,3 +9,9 @@ function adjust_font(amount) {
   $("card_back_value").setStyle({fontSize: "" + (start_size + amount) + "px"});
 }
 
+function gaLoad() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+}
+
